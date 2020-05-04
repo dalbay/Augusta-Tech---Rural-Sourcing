79f690834500cd086708f93d51f31e7db86d3448
dalbay <fadalbay@gmail.com>
2020-05-03 21:54:30 -0400

    category validation

--- AugTechRSI/ClientApp/src/app/category/category.component.ts
+++ AugTechRSI/ClientApp/src/app/category/category.component.ts
@@ -12,11 +12,15 @@ export class CategoryComponent implements OnInit {
 
     public categories: Category[] = [];
 
+    //new cateogry
+    category: any;
 
     constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
         http.get(baseUrl + 'api/Category').subscribe(result => {
             this.categories = result as Category[];
         }, error => console.error(error));
+
+        this.category = new NewCategory();
     }
     ngOnInit() { }
 }
@@ -25,4 +29,11 @@ interface Category {
     typeId: number;
     typeName: string;
     typeDescription: string;
+}
+
+class NewCategory {
+            typeId: number;
+        typeName: string;
+    typeDescription: string;
+    constructor( ) { }
 }
